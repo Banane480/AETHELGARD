5151abe552d774c6b0cec062caa46233bd3ae3db
babou08 <remybarbaro068@gmail.com>
2026-09-18 11:24:29 +0200

    Tache 21 : création de la fonction CharacterTurn

--- src/combat.go
+++ src/combat.go
@@ -1,2 +1,35 @@
 package main
 
+import "fmt"
+
+func CharacterTurn(c Character, m Monster) (Character, Monster) {
+	for {
+		var choice int
+
+		fmt.Println("\n--- C'EST À VOUS DE JOUER ---")
+		fmt.Println("Que voulez-vous faire ?")
+		fmt.Println("1 : Attaque basique")
+		fmt.Println("2 : Utilisez un objet de votre inventaire")
+		fmt.Print("Votre choix : ")
+
+		fmt.Scan(&choice)
+		switch choice {
+		case 1:
+			damage := 8
+			m.Life -= damage
+			if m.Life < 0 {
+				m.Life = 0
+			}
+			fmt.Printf(" %s attaque et inflige %d dégâts à %s !\n", c.Name, damage, m.Name)
+			fmt.Printf("Il reste %d/%d PV à %s.\n", m.Life, m.LifeMax, m.Name)
+			return c, m
+
+		case 2:
+			fmt.Println("Accès à l'inventaire en combat...")
+			return c, m
+
+		default:
+			fmt.Println("Choix invalide, veuillez réessayer.")
+		}
+	}
+}
--- src/monster.go
+++ src/monster.go
@@ -9,12 +9,12 @@ type Monster struct {
 	Attaque int
 }
 
-func InitGoblin(Name string, LifeMax int, Life int, Attaque int) Monster {
+func InitGoblin(name string, lifeMax int, life int, attaque int) Monster {
 	return Monster{
-		Name:    Name,
-		LifeMax: LifeMax,
-		Life:    Life,
-		Attaque: Attaque,
+		Name:    name,
+		LifeMax: lifeMax,
+		Life:    life,
+		Attaque: attaque,
 	}
 }
 
@@ -22,15 +22,15 @@ func GoblinPattern(m Monster, c Character, turn int) Character {
 	damage := m.Attaque
 	if turn%3 == 0 {
 		damage *= 2
-		fmt.Printf("Coup critique ! %s utilise une attaque puissante et inflige %s dégats à %s", m.Name, damage, c.Name)
+		fmt.Printf("Coup critique ! %s utilise une attaque puissante et inflige %d dégats à %s", m.Name, damage, c.Name)
 	} else {
-		fmt.Printf("%s attaque %s et inflige %s dégats", m.Name, c.Name, damage)
+		fmt.Printf("%s attaque %s et inflige %d dégats", m.Name, c.Name, damage)
 	}
 	c.CurrentHP -= damage
 	if c.CurrentHP < 0 {
 		c.CurrentHP = 0
 	}
-	fmt.Printf("Il reste %d/%d PV à %s.\n", c.CurrentHP, c.MaxHP)
+	fmt.Printf("Il reste %d/%d PV à %s.\n", c.CurrentHP, c.MaxHP, c.Name)
 
 	return c
 }
