f994b2a0de22323ad793f6a1002e40f48d347266
aidan <aidan.roche@ynov.com>
2026-09-18 10:30:56 +0200

    Tâche 2 : ajout de la fonction InitCharacter et test dans le main

--- src/main.go
+++ src/main.go
@@ -1,2 +1,9 @@
 package main
 
+import "fmt"
+
+func main() {
+	perso := InitCharacter("Aventurier", "Humain", 1, 100, 50, []string{"Potion de soin", "Potion de soin"})
+
+	fmt.Printf("Personnage créé : %+v\n", perso)
+}
