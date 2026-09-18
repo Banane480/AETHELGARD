c8b2a83babbc5a4d471321f01d17e9db1176c2b4
babou08 <remybarbaro068@gmail.com>
2026-09-18 10:39:06 +0200

    Tache 19 : création de la fonction InitGoblin

--- src/monster.go
+++ src/monster.go
@@ -6,3 +6,12 @@ type Monster struct {
 	Life    int
 	Attaque int
 }
+
+func InitGoblin(Name string, LifeMax int, Life int, Attaque int) Monster {
+	return Monster{
+		Name:    Name,
+		LifeMax: LifeMax,
+		Life:    Life,
+		Attaque: Attaque,
+	}
+}
