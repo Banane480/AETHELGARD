367887f5e72e4f1662bda7fefcd96910e5ad072d
aidan <aidan.roche@ynov.com>
2026-09-18 10:48:04 +0200

    Tâche 4 : ajout de la méthode AccessInventory pour consulter l'inventaire

--- src/main.go
+++ src/main.go
@@ -1,7 +1,15 @@
 package main
 
-func main() {
-	perso := InitCharacter("Aventurier", "Humain", 1, 100, 50, []string{"Potion de soin", "Potion de soin"})
+import "fmt"
 
-	perso.DisplayInfo()
+func main() {
+	// Test 1 : avec des objets
+	perso := InitCharacter("Aventurier", "Humain", 1, 100, 50, []string{"Potion de soin", "Potion de soin"})
+	fmt.Println("--- Test avec objets ---")
+	perso.AccessInventory()
+
+	// Test 2 : avec inventaire vide
+	persoVide := InitCharacter("Guerrier", "Nain", 1, 100, 50, []string{})
+	fmt.Println("\n--- Test inventaire vide ---")
+	persoVide.AccessInventory()
 }
