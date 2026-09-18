d6c6c6564af39133cea03e2323a55412d9caf99e
aidan <aidan.roche@ynov.com>
2026-09-18 10:39:27 +0200

    Tâche 3 : ajout de la méthode DisplayInfo pour afficher le personnage

--- src/character.go
+++ src/character.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Character struct {
 	Name      string
 	Class     string
@@ -18,4 +20,17 @@ func InitCharacter(name string, class string, level int, maxHP int, currentHP in
 		CurrentHP: currentHP,
 		Inventory: inventory,
 	}
+}
+
+func (c Character) DisplayInfo() {
+	fmt.Println("--- FICHE DU PERSONNAGE ---")
+	fmt.Printf("Nom      : %s\n", c.Name)
+	fmt.Printf("Classe   : %s\n", c.Class)
+	fmt.Printf("Niveau   : %d\n", c.Level)
+	fmt.Printf("PV       : %d / %d\n", c.CurrentHP, c.MaxHP)
+	fmt.Println("Inventaire :")
+	for _, item := range c.Inventory {
+		fmt.Printf(" - %s\n", item)
+	}
+	fmt.Println("---------------------------")
 }
